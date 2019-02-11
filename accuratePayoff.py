ea5ddf8afd6c2e2755a152f2b0633875ad48e1c4
davebza <daveb.za@gmail.com>
2019-02-11 12:48:23 +0800

    Week 2: problem sets revised

--- accuratePayoff.py
+++ accuratePayoff.py
@@ -1,13 +1,9 @@
-balance = 320000
-annualInterestRate = 0.2
-expectedAnswer = 29157.09
-
 def newBalance(balance, monthlyInterest, minimumPayment):
 
     workingBalance = balance - minimumPayment
     workingBalance = workingBalance + (workingBalance * monthlyInterest)
 
-    return round(workingBalance, 2)
+    return workingBalance
 
 def testMini(balance):
 
@@ -18,28 +14,31 @@ def testMini(balance):
 
     return balance
 
+# Values:
+
+balance = 320000
+annualInterestRate = 0.2
+expectedAnswer = 29157.09
 monthlyInterest = annualInterestRate /12
+
 lowerBound = balance / 12
 upperBound = (balance * (1+monthlyInterest)**12)/12
-print(f"Upper {upperBound}")
 
-minimumPayment = 0
+
+print(f"Lower: {lowerBound}")
+print(f"Upper: {upperBound}")
+
+minimumPayment = upperBound/2
 testedBalance = balance
+count = 0
 
-
-# count = 0
-# while count < 12:
-#     count += 1
-#     balance = newBalance(balance, monthlyInterest, minimumPayment)
-
-while testedBalance > 0:
-    minimumPayment += 10
-    testedBalance = testMini(balance)
-    #print(testedBalance)
-
-print("Lowest payment: "+ str(minimumPayment))
+print(f"Minimum: {minimumPayment}")
+testedBalance = testMini(balance)
+print(f"New balance: {testedBalance}")
 
 if expectedAnswer == minimumPayment:
     print("Hey")
 else:
     print("No!")
+
+print(count)
