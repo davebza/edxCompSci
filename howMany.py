89790b0230bd278e3a411480586ce72d1e9b6ac3
davebza <daveb.za@gmail.com>
2019-02-13 18:54:34 +0800

    week 3: problem set begins

--- howMany.py
+++ howMany.py
@@ -14,25 +14,6 @@ def how_many(aDict):
 
     return count
 
-def biggest(aDict):
-    '''
-    aDict: A dictionary, where all the values are lists.
 
-    returns: The key with the largest number of values associated with it
-    '''
-    # Your Code Here
-
-    currentBiggest = 0
-    count = 0
-    currentKey = ""
-
-    for key in aDict:
-        current = aDict.get(key)
-        count = len(current)
-        if count > currentBiggest:
-            currentBiggest = count
-            currentKey = key
-
-    return currentKey
 
 
